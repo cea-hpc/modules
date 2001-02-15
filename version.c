689fb748f8ac134e35ab1f8fba26141b48f0a63f
rminsk <rminsk>
2001-02-15 02:40:56 +0000

    Retracted new version

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.1.2";
+const char *version_string = "3.1.1";
 const char *date_string = "14FEB01";
-/* NEW TAG "modules-3-1-2" */
+/* NEW TAG "modules-3-1-1" */
 /* OLD TAG "modules-3-1-0" */
