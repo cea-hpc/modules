f5437378064684bb28b01bdb77a8c86024ba359e
rminsk <rminsk>
2001-02-15 02:39:53 +0000

    Bumped version for mel addition

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.1.1";
-const char *date_string = "01FEB01";
-/* NEW TAG "modules-3-1-1" */
+const char *version_string = "3.1.2";
+const char *date_string = "14FEB01";
+/* NEW TAG "modules-3-1-2" */
 /* OLD TAG "modules-3-1-0" */
