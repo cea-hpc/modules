2e30702aecfd734e02e8686155e50f1d24a38ade
rkowen <rkowen>
2001-08-03 19:59:32 +0000

    * Added bz2 files to distribution mix.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.1.3.2";
-const char *date_string = "2001-07-25";
-/* NEW TAG "modules-3-1-3-2" */
+const char *version_string = "3.1.4";
+const char *date_string = "2001-08-03";
+/* NEW TAG "modules-3-1-4" */
 /* OLD TAG "modules-3-1-3" */
