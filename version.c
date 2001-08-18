0f6130439597e4a4e4aee3fa7c832ff895e1ce19
rkowen <rkowen>
2001-08-18 14:36:16 +0000

    Release 3.1.5

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.1.4.2";
-const char *date_string = "2001-08-09";
-/* NEW TAG "modules-3-1-4" */
-/* OLD TAG "modules-3-1-3" */
+const char *version_string = "3.1.5";
+const char *date_string = "2001-08-18";
+/* NEW TAG "modules-3-1-5" */
+/* OLD TAG "modules-3-1-4" */
