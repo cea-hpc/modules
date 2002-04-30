5340e1459c26e0f4112dfc2db32d52f951eedc19
rkowen <rkowen>
2002-04-30 19:32:57 +0000

    Moved install.sh to config/ and updated the version to 3.2.0a .

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.1.7a";
-const char *date_string = "2001-11-07";
-/* NEW TAG "modules-3-1-7a" */
+const char *version_string = "3.2.0a";
+const char *date_string = "2002-05-07";
+/* NEW TAG "modules-3-2-0a" */
 /* OLD TAG "modules-3-1-6" */
