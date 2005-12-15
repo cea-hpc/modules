c05002c5deaab3eb79af496772037cc5e539535b
rkowen <rkowen>
2005-12-15 17:35:21 +0000

    Time for the 3.2.0 release

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0rc3";
-const char *date_string = "2005-11-28";
-/* NEW TAG "modules-3-2-0rc3" */
+const char *version_string = "3.2.0";
+const char *date_string = "2005-12-15";
+/* NEW TAG "modules-3-2-0" */
 /* OLD TAG "modules-3-1-6" */
