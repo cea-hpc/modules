ac2ac65f13799e56526ff34f8d563ce0674206d7
rkowen <rkowen>
2006-02-04 21:54:31 +0000

    The current version for the i18n mods is 3.3 alpha

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0";
-const char *date_string = "2006-01-17";
-/* NEW TAG "modules-3-2-0" */
-/* OLD TAG "modules-3-1-6" */
+const char *version_string = "3.3.a";
+const char *date_string = "2006-02-03";
+/* NEW TAG "modules-3-3-a" */
+/* OLD TAG "modules-3-2-0" */
