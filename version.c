6ed2875c585bec2bcab2179739be6f7266862fc3
R.K. Owen <rk@owen.sj.ca.us>
2012-10-25 12:13:36 -0700

    Check whether the bash shell is interactive or not Fixed a typo in the perl init script Trivial change to the version variable.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.9";
-const char *date_string = "2011-11-24";
+const char *version_string = "3.2.10";
+const char *date_string = "2012-10-31";
 /* NEW TAG "modules-3-2-8" */
 /* OLD TAG "modules-3-2-7" */
