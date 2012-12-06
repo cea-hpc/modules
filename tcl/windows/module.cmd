f7da10859e503cdf813f50f2423d8bb5ca24ab71
R.K. Owen <rk@owen.sj.ca.us>
2012-12-06 14:21:40 -0800

    The Tcl-only version of modules has been forked to modules-tcl.

--- tcl/windows/module.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set OUTPUT=
-set MODCMD="call tclsh %MODULESHOME%\modulecmd.tcl cmd %*"
-REM -- Each output line needs to be executed in cmd shell
-for /f "tokens=*" %%I in ('%MODCMD%') do %%I
--- tcl/windows/moduleinit.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-REM -----------------------------------------------
-REM Script to customize module-tcl for "cmd" shell
-REM Load the modulepaths with "module use"
-REM -----------------------------------------------
-call module use %MODULESHOME%\modulefiles
-call module avail
