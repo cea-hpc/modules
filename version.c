bcdfda9c835f1915a13c1f8ffdd2f6af4bd8e907
R.K. Owen <rk@owen.sj.ca.us>
2012-12-20 11:38:28 -0800

    Updated the version date.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 const char *version_string = "3.2.10";
-const char *date_string = "2012-10-31";
+const char *date_string = "2012-12-21";
 /* NEW TAG "modules-3-2-8" */
 /* OLD TAG "modules-3-2-7" */
