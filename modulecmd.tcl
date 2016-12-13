8e050c4123c33f0801c00620db360cfb4fd7c476
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2016-12-13 06:48:49 +0100

    Fix perl quoting style for variable set
    Import 'setenv-eschars' test in 50-cmds suite and by the way fix quoting
    style for perl when setting variable. Single quotes are used to set
    variable, so single quotes should be escaped rather than double quote.

--- modulecmd.tcl
+++ modulecmd.tcl
@@ -20,7 +20,7 @@ echo "FATAL: module: Could not find tclsh in \$PATH or in standard directories" 
 #
 # Some Global Variables.....
 #
-set MODULES_CURRENT_VERSION 1.661
+set MODULES_CURRENT_VERSION 1.662
 set g_debug 0 ;# Set to 1 to enable debugging
 set error_count 0 ;# Start with 0 errors
 set g_autoInit 0
@@ -1854,7 +1854,7 @@ proc renderSettings {} {
                puts stdout "set env($var) $val;"
             }
             perl {
-               set val [doubleQuoteEscaped $env($var)]
+               set val [singleQuoteEscaped $env($var)]
                puts stdout "\$ENV{\'$var\'} = \'$val\';"
             }
             python {
