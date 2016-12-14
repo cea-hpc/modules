d4eab72ab3c74c7197819373028bc9b1bc470ce0
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2016-12-14 20:19:56 +0100

    Improve unsetenv for unload and display modes
    On unload mode, delete environment variable targeted by an unsetenv
    directive if no value has been provided along.
    
    On display mode, print environment variable value if any has been passed
    to unsetenv directive.
    
    Import 025-setenv-undo, 035-unsetenv-undo and 036-unsetenv-x tests in
    50-cmds suite.

--- modulecmd.tcl
+++ modulecmd.tcl
@@ -20,7 +20,7 @@ echo "FATAL: module: Could not find tclsh in \$PATH or in standard directories" 
 #
 # Some Global Variables.....
 #
-set MODULES_CURRENT_VERSION 1.663
+set MODULES_CURRENT_VERSION 1.664
 set g_debug 0 ;# Set to 1 to enable debugging
 set error_count 0 ;# Start with 0 errors
 set g_autoInit 0
@@ -901,10 +901,16 @@ proc unsetenv {var {val {}}} {
       if {$val ne ""} {
          set env($var) $val
          set g_stateEnvVars($var) "new"
+      } else {
+         set g_stateEnvVars($var) "del"
       }
    }\
    elseif {$mode eq "display"} {
-      report "unsetenv\t\t$var"
+      if {$val ne ""} {
+         report "unsetenv\t\t$var\t$val"
+      } else {
+         report "unsetenv\t\t$var"
+      }
    }
    return {}
 }
