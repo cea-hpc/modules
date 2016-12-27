9b87b9c74129f4ba24ad2240f66b8708c0f49ee3
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2016-12-27 12:49:01 +0100

    Make 'switch' command consistent
    Switch command were previsouly setting 'new' as 'old' and 'old' as 'new'
    when 'new' is detected loaded. This behavior led to inconsistency
    especially when the same 'module switch old new' command is called
    twice. First time 'old' is loaded and 'new' is unloaded. Then for the
    second call 'old' is unloaded and 'new' is loaded, and so on. In this
    case, the result of the switch command depends on the current situation
    which confuses most persons using it.
    
    This commit makes the switch command idempotent by always ending up with
    'old' unloaded and 'new' loaded, whatever the starting situation is.

--- modulecmd.tcl
+++ modulecmd.tcl
@@ -20,7 +20,7 @@ echo "FATAL: module: Could not find tclsh in \$PATH or in standard directories" 
 #
 # Some Global Variables.....
 #
-set MODULES_CURRENT_VERSION 1.675
+set MODULES_CURRENT_VERSION 1.676
 set g_debug 0 ;# Set to 1 to enable debugging
 set error_count 0 ;# Start with 0 errors
 set g_autoInit 0
@@ -3281,10 +3281,6 @@ proc cmdModuleSwitch {old {new {}}} {
 
    if {$new eq ""} {
       set new $old
-   } elseif {[info exists g_loadedModules($new)]} {
-      set tmp $new
-      set new $old
-      set old $tmp
    }
 
    if {![info exists g_loadedModules($old)] &&
