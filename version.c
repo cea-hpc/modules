15c9b8bcf876ab49f0690b313df6c00c30588161
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2017-10-16 06:28:38 +0200

    Release of version 3.2.11

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
 const char *version_string = "3.2.11";
-const char *date_string = "2017-XX-XX";
+const char *date_string = "2017-10-16";
 /* NEW TAG "modules-3-2-8" */
 /* OLD TAG "modules-3-2-7" */
