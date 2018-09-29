07b5278296b7b95f7d6d505ed2b2ac0e233b65af
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2018-09-29 16:29:46 +0200

    cookbook: add 'unload-firstly-loaded' recipe
    Helpful for #198

---
+++ contrib/cookbook/unload-firstly-loaded/siteconfig.tcl
@@ -0,0 +1,22 @@
+#
+# siteconfig.tcl - Site specific configuration changing getLoadedMatchingName
+#   procedure to return firstly loaded module by default instead of lastly
+#   loaded module. With this the firsly loaded module will be unloaded instead
+#   of the lastly loaded in case multiple loaded modules match passed name to
+#   unload. Which was the behavior of Modules v3.2 (compatbility) version.
+#
+# Author: Xavier Delaruelle <xavier.delaruelle@cea.fr>
+# Compatibility: Modules v4.1
+#
+# Installation: put this file in the 'etc' directory of your Modules
+#   installation. Refer to the "Modulecmd startup" section in the
+#   module(1) man page to get this location.
+
+# override 'getLoadedMatchingName' procedure to set behavior argument to the
+# 'returnfirst' value by default
+rename ::getLoadedMatchingName ::__getLoadedMatchingName
+proc getLoadedMatchingName {name {behavior returnfirst}} {
+   return [__getLoadedMatchingName $name $behavior]
+}
+
+# vim:set tabstop=3 shiftwidth=3 expandtab autoindent:
