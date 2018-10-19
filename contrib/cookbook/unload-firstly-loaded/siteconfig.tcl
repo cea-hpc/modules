70ec8ae0a0a30a3e833c05a734d8866847c2f378
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2018-10-19 07:49:11 +0200

    cookbook: port 'unload-firstly-loaded' to v4.2

--- contrib/cookbook/unload-firstly-loaded/siteconfig.tcl
+++ contrib/cookbook/unload-firstly-loaded/siteconfig.tcl
@@ -6,7 +6,7 @@
 #   unload. Which was the behavior of Modules v3.2 (compatbility) version.
 #
 # Author: Xavier Delaruelle <xavier.delaruelle@cea.fr>
-# Compatibility: Modules v4.1
+# Compatibility: Modules v4.2
 #
 # Installation: put this file in the 'etc' directory of your Modules
 #   installation. Refer to the "Modulecmd startup" section in the
@@ -15,8 +15,8 @@
 # override 'getLoadedMatchingName' procedure to set behavior argument to the
 # 'returnfirst' value by default
 rename ::getLoadedMatchingName ::__getLoadedMatchingName
-proc getLoadedMatchingName {name {behavior returnfirst}} {
-   return [__getLoadedMatchingName $name $behavior]
+proc getLoadedMatchingName {name {behavior returnfirst} {loading 0}} {
+   return [__getLoadedMatchingName $name $behavior $loading]
 }
 
 # vim:set tabstop=3 shiftwidth=3 expandtab autoindent:
