1f73faf8c072d2ecf0b80c12fa81823e041433b2
Jacques Raphanel <dvp@crabisoft.fr>
2019-05-08 04:02:44 +0200

    Add support for portable/standalone install on Windows
    Update moduleinit.cmd file to set Modules-specific environment variables
    according to the script directory and the file existence for:
    
    * MODULESHOME if not defined yet and if `..\libexec\modulecmd.tcl` exists
    * add `..\bin` directory to PATH if `module.cmd` is not available from
      PATH and available from `..\bin`
    * add `..\bin` directory to PATH if `tclsh.exe` is not available from
      PATH and available from `..\bin`
    
    Automatically call `moduleinit.cmd` from `module.cmd` if modules is not
    initialized yet (based on environment variables)
    
    Fixes #272

--- windows/moduleinit.cmd
+++ windows/moduleinit.cmd
@@ -3,5 +3,35 @@ REM -----------------------------------------------
 REM Script to customize module-tcl for "cmd" shell
 REM Load the modulepaths with "module use"
 REM -----------------------------------------------
+
+:: Define MODULESHOME is defined yet
+if defined MODULESHOME goto:skipInit
+if not exist "%~dp0..\libexec\modulecmd.tcl" goto:skipInit
+for %%i in ("%~dp0..") do set "MODULESHOME=%%~dpni"
+
+:skipInit
+
+:: check if module.cmd directory needs to be added to PATH
+if not exist "%~dp0module.cmd" goto:skipModuleInit
+for %%i in (module.cmd) do ( 
+    if "x%%~$PATH:i" EQU "x" (
+        for %%a in (%~dp0.) do set "PATH=%%~dpnxa;%PATH%"
+    )
+)
+
+:skipModuleInit
+
+:: check if tclsh.exe directory must be added to PATH
+if not exist "%~dp0..\bin\tclsh.exe" goto:skipTclShInit
+for %%i in (tclsh) do ( 
+    if "x%%~$PATH:i" EQU "x" (
+        for %%a in (%~dp0.) do set "PATH=%%~dpnxa;%PATH%"
+    )
+)
+
+:skipTclShInit
+
+if not defined MODULES_CMD set MODULES_CMD=%~dp0..\libexec\modulecmd.tcl
 call module use %MODULESHOME%\modulefiles
-call module avail
+:: Display available modules only if no input argument
+if "%~1" EQU "" call module avail
