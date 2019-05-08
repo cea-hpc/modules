cc3532b8c3fb7dc86c82b109fa197ea6b44019ab
Jacques Raphanel <dvp@crabisoft.fr>
2019-05-08 04:02:44 +0200

    Add support for portable/standalone install on Windows
    Update moduleinit.cmd file to set Modules-specific environment variables
    according to the script directory and the file existence for:
    
    * MODULESHOME if not defined yet and if `..\libexec\modulecmd.tcl` exists
    * add `..\bin` directory to PATH if `module.cmd` is not available from
      PATH and available from `..\bin`
    * add `..\bin` directory to PATH if `tclsh.exe` is not available from
      PATH and available from `..\bin`
    
    Automatically call `moduleinit.cmd` from `module.cmd` if modules is not
    initialized yet (based on environment variables)
    
    Fixes #272

--- windows/module.cmd
+++ windows/module.cmd
@@ -1,5 +1,22 @@
 @echo off
+:: Initialize Modules if already done yet
+if not defined MODULESHOME goto:initModules
+if not defined MODULES_CMD goto:initModules
+if "%MODULESHOME%\bin\module.cmd" NEQ "%~dpnx0" goto:initModules
+goto:skipInit
+
+:initModules
+call "%~dp0moduleinit.cmd" %*
+
+:skipInit
 set OUTPUT=
-set MODCMD="call tclsh %MODULESHOME%\modulecmd.tcl cmd %*"
+set MODCMD="call tclsh %MODULES_CMD% cmd %*"
 REM -- Each output line needs to be executed in cmd shell
 for /f "tokens=*" %%I in ('%MODCMD%') do %%I
+goto:Done
+
+:reportError
+echo Modules is not initialized yet. Please call init\profile.cmd from your
+echo Modules installation directory before to use module command.
+
+:Done
