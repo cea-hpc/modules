a7be90ae993a380ae659e2c14b7c0a62541300a7
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2019-08-04 09:43:05 +0200

    Update siteconfig.tcl files with new int config setup ways
    Use the lappendConf procedure to set the locked_configs option and the
    setConf procedure to set the avail_report_dir_sym and
    avail_report_mfile_sym options.

--- siteconfig.tcl
+++ siteconfig.tcl
@@ -8,9 +8,9 @@
 
 # uncomment the following line to forbid the definition of an extra
 # site-specific configuration script
-#appendNoDupToList g_lockedconfigs extra_siteconfig
+#lappendConf locked_configs extra_siteconfig
 
 # uncomment the following line to forbid `implicit_default` config option
 # superseding
-#appendNoDupToList g_lockedconfigs implicit_default
+#lappendConf locked_configs implicit_default
 
