7b2911c4a138a454b96010c067b3cfdb0e3af2dc
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2020-01-13 10:04:04 +0100

    script: simplify module.cmd batch file
    Execute initialization script if MODULES_CMD environment variable is not
    found defined.

--- script/module.cmd
+++ script/module.cmd
@@ -1,22 +1,11 @@
 @echo off
-:: Initialize Modules if already done yet
-if not defined MODULESHOME goto:initModules
-if not defined MODULES_CMD goto:initModules
-if "%MODULESHOME%\bin\module.cmd" NEQ "%~dpnx0" goto:initModules
-goto:skipInit
 
-:initModules
-call "%~dp0..\init\cmd.cmd" %*
+:: initialize Modules if not already done
+if not defined MODULES_CMD (
+   call %~dp0..\init\cmd.cmd
+)
 
-:skipInit
 set OUTPUT=
 set MODCMD="call tclsh %MODULES_CMD% cmd %*"
-REM -- Each output line needs to be executed in cmd shell
+:: execute each output line in cmd shell
 for /f "tokens=*" %%I in ('%MODCMD%') do %%I
-goto:Done
-
-:reportError
-echo Modules is not initialized yet. Please call init\profile.cmd from your
-echo Modules installation directory before to use module command.
-
-:Done
