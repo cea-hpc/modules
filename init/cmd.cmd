a5f3e842f9052586566acb37d72b66a12ad7c8da
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2020-01-13 09:57:54 +0100

    init: simplify cmd init file
    Always define Modules-specific environment variables when running `cmd`
    initialization file.

--- init/cmd.cmd
+++ init/cmd.cmd
@@ -1,37 +1,15 @@
 @echo off
-REM -----------------------------------------------
-REM Script to customize module-tcl for "cmd" shell
-REM Load the modulepaths with "module use"
-REM -----------------------------------------------
 
-:: Define MODULESHOME is defined yet
-if defined MODULESHOME goto:skipInit
-if not exist "%~dp0..\libexec\modulecmd.tcl" goto:skipInit
-for %%i in ("%~dp0..") do set "MODULESHOME=%%~dpni"
+:: define Modules-specific environment variables
+for %%i in (%~dp0..) do set MODULESHOME=%%~dpni
+set MODULES_CMD=%MODULESHOME%\libexec\modulecmd.tcl
 
-:skipInit
-
-:: check if module.cmd directory needs to be added to PATH
-if not exist "%~dp0..\bin\module.cmd" goto:skipModuleInit
-for %%i in (module.cmd) do ( 
-    if "x%%~$PATH:i" EQU "x" (
-        for %%a in (%~dp0.) do set "PATH=%%~dpnxa;%PATH%"
-    )
+:: ensure module.cmd batch file can be found in PATH
+for %%i in (module.cmd) do (
+   if "x%%~$PATH:i" EQU "x" (
+      set PATH=%MODULESHOME%\bin;%PATH%
+   )
 )
 
-:skipModuleInit
-
-:: check if tclsh.exe directory must be added to PATH
-if not exist "%~dp0..\bin\tclsh.exe" goto:skipTclShInit
-for %%i in (tclsh) do ( 
-    if "x%%~$PATH:i" EQU "x" (
-        for %%a in (%~dp0.) do set "PATH=%%~dpnxa;%PATH%"
-    )
-)
-
-:skipTclShInit
-
-if not defined MODULES_CMD set MODULES_CMD=%~dp0..\libexec\modulecmd.tcl
+:: enable a default modulepath directory
 call module use %MODULESHOME%\modulefiles
-:: Display available modules only if no input argument
-if "%~1" EQU "" call module avail
