fd7ba8de87107b5a2b07adfcdf7dad4c777c3b3a
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2020-06-24 07:50:09 +0200

    doc: include logo in doc portal

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -127,6 +127,20 @@ else:
 # documentation.
 #
 # html_theme_options = {}
+if os_rtd:
+    html_theme_options = { 'logo_only' : True }
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+if os_rtd:
+    html_logo = '../img/modules_white.svg'
+else:
+    html_logo = '../img/modules_red.svg'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '../img/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
