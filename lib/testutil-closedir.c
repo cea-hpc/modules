1e7a098e7ec89e795efcf0b83bb73e5acf9a4bbc
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2021-02-28 19:07:36 +0100

    ts: re-license test libraries from GPLv3+ to GPLv2+
    Change license of all testutil-* libraries, used to test Modules Tcl
    extension library, from GPLv3+ to GPLv2+.
    
    Align this way all files from the Modules project under the GPLv2+
    license.
    
    Consent has been obtained from copyright holder to perform such move (I
    hold the full copyright for these librairies).
    
    Related to #389

--- lib/testutil-closedir.c
+++ lib/testutil-closedir.c
@@ -1,11 +1,11 @@
 /*************************************************************************
  *
  * TESTUTIL-CLOSEDIR.C, Superseded closedir function for test purpose
- * Copyright (C) 2019 Xavier Delaruelle
+ * Copyright (C) 2019-2021 Xavier Delaruelle
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
