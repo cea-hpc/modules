7418b3fce01931a8b64058ba07ae80586d45f014
Lukáš Zaoral <lzaoral@redhat.com>
2022-08-26 17:53:45 +0200

    lint(ShellCheck): fix SC2128 warning
    These occurrences do not cause any problems described below but it's better
    to be verbose that we indeed want to access the first array element.
    
    According to ShellCheck's documentation:
    
    When referencing arrays, $myarray is equivalent to ${myarray[0]} --
    it results in only the first of multiple elements.
    
    Related: https://www.shellcheck.net/wiki/SC2128
    Related: #470

--- doc/example/source-script-in-modulefile/bar-2.1/bar-setup.sh
+++ doc/example/source-script-in-modulefile/bar-2.1/bar-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$(dirname $BASH_SOURCE)/bin:$PATH
+export PATH=$(dirname "${BASH_SOURCE[0]}")/bin:$PATH
 bar() {
     barbin -q -l
 }
--- doc/example/source-script-in-modulefile/foo-1.2/foo-setup.sh
+++ doc/example/source-script-in-modulefile/foo-1.2/foo-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FOOENV="$1"
-export PATH=$(dirname $BASH_SOURCE)/bin:$PATH                                                             
+export PATH=$(dirname "${BASH_SOURCE[0]}")/bin:$PATH
 alias foo='foobin -q -l'
