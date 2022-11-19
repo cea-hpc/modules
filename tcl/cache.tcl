9ceb82e9146b44a873309143717e7ed7f78a46d2
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2022-11-18 19:26:59 -0600

    Core code for formatModuleCacheContent
    Implement formatModuleCacheContent procedure. findModules procedure is
    first called to get all files from designated modulepath directory. A
    cache entry is made for each result obtained from findModules with
    cache file commands: modulefile-content, modulerc-content and
    modulefile-invalid.
    
    Goal is to record all information produced by findModules to be able to
    replace findModules processing by cache file evaluation. Thus modulefile
    modification time and validity check issues are recorded in cache file.
    
    Modulerc and modulefile content is read and recorded as-is in cache
    file. Error occurring during file read is raised and ends cache file
    creation.

--- tcl/cache.tcl
+++ tcl/cache.tcl
@@ -25,6 +25,57 @@ proc getModuleCacheFilename {modpath} {
 
 # Build cache file content for given modulepath
 proc formatModuleCacheContent {modpath} {
+   set content {}
+   # collect files from modulepath directory
+   array set found_list [findModules $modpath * 0 1]
+
+   # build cache entry for every file found
+   foreach elt [lsort [array names found_list]] {
+      set entry_list [list]
+      set fetch_content 0
+      switch -- [lindex $found_list($elt) 0] {
+         modulerc {
+            lappend entry_list modulerc-content $elt
+            set fetch_content 1
+         }
+         modulefile {
+            lappend entry_list modulefile-content $elt [lindex\
+               $found_list($elt) 1]
+            set fetch_content 1
+         }
+         default {
+            # also record obtained error to get all the information to cover
+            # everything fetched by findModules. only modulefile validity is
+            # checked in findModules
+            lappend entry_list modulefile-invalid $elt {*}[lrange\
+               $found_list($elt) 0 1]
+         }
+      }
+      # fetch file content
+      if {$fetch_content} {
+         if {[catch {
+            set fcontent [readFile $modpath/$elt]
+            # extract module header from the start of the file
+            if {![regexp {^#%Module[0-9\.]*} [string range $fcontent 0 32]\
+               fheader]} {
+               set fheader {}
+            }
+            lappend entry_list $fheader $fcontent
+         } errMsg]} {
+            # rethrow read error after parsing message
+            knerror [parseAccessIssue $modpath/$elt]
+         }
+      }
+      # format cache entry
+      append content "\n$entry_list"
+   }
+
+   # prepend header if some content has been generated
+   if {[string length $content] != 0} {
+      set content "#%Module$content"
+   }
+
+   return $content
 }
 
 # ;;; Local Variables: ***
